238402c71c3a73ab50540dbee3b1ca9259e5daba
carlalaya <carla.laya@gmail.com>
2024-02-19 21:03:27 -0300

    HTML y CSS Funcion encriptar y desencriptar

---
+++ javascrip.js
@@ -0,0 +1,44 @@
+function encriptar() {
+    let texto = document.getElementById("input-text").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+  
+    let textoCifrado = texto
+      .replace(/e/gi, "enter")
+      .replace(/i/gi, "imes")
+      .replace(/a/gi, "ai")
+      .replace(/o/gi, "ober")
+      .replace(/u/gi, "ufat");
+  
+    if (texto.length != 0) {
+      document.getElementById("input-text").value = textoCifrado;
+      tituloMensaje.textContent = "Texto encriptado con éxito";
+    } else {
+
+      tituloMensaje.textContent = "¡ Atención ! Ningún mensaje fue encontrado";
+    }
+  };
+  
+  function desencriptar() {
+    let texto = document.getElementById("input-text").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+
+    let textoCifrado = texto
+      .replace(/enter/gi, "e")
+      .replace(/imes/gi, "i")
+      .replace(/ai/gi, "a")
+      .replace(/ober/gi, "o")
+      .replace(/ufat/gi, "u");
+    
+      if (texto.length != 0) {
+        document.getElementById("input-text").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        //parrafo.textContent = "";
+      } else {
+        tituloMensaje.textContent = "¡ Atención ! Ningún mensaje fue encontrado";
+    }
+  };
+
+function borrarTexto (){
+ document.querySelector('#input-text').value = '';
+};
+
